be80dbc80b288f83aec064f446b39fc99f93911b
Ayush Pratap Singh <ayushdce2@gmail.com>
2025-01-12 11:16:18 +0530

    add

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -8,7 +8,7 @@ const RestaurantCard = (props)=>{
     return(
         <div className="res-card" style={styleCard}>
             <img alt="food"className="res-logo" src={CDN_URL+cloudinaryImageId}/>
-            <h3>{name}</h3>
+            <h3>{name} auiuiui</h3>
             <h4>{cuisines.join(" , ")}</h4>
             <h4>{avgRating}</h4>
             <h4>{slaString}</h4>
